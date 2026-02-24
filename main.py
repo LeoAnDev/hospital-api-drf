5c1b954db7cbd3cc0b467370c95aa5b62dd6f301
Leonardo de Andrade Oliveira <leonardo.esportemental@gmail.com>
2026-02-24 20:20:58 -0300

    Update main.py to enhance documentation and remove unused function

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 """
-_summary_
+This is a simple Python script that prints "Hello World!" to the console.
+It serves as a basic example of how to write and run a Python program.
+The code consists of a single line that uses the built-in `print`
+function to output the string "Hello World!" when executed.
+This is often used as a first program for beginners learning to code in
+Python.
 """
 
-
-def test_somethindg():
-    """
-    _summary_
-    """
-    assert 1 == 1
+print('Hello World!')
