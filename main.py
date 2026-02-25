017143c39cfa86e936d2c6df282659f831163e86
Leonardo De Andrade Oliveira <leonardo.esportemental@gmail.com>
2026-02-25 18:33:18 -0300

    Remove detailed description from main.py docstring

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 """
-This is a simple Python script that prints "Hello World!" to the console.
-It serves as a basic example of how to write and run a Python program.
-The code consists of a single line that uses the built-in `print`
-function to output the string "Hello World!" when executed.
-This is often used as a first program for beginners learning to code in
-Python.
+Test file
 """
 
 print('Hello World!')
